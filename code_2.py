4fbd5bec071903a4a071134695fa1da19ebe3577
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2024-03-05 15:34:15 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,17 @@
+
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+function generateRandomData() {
+        console.log("Random Number: " + item);
+}
+main();
+
+    data.forEach(item => {
+
+    }
+    });
+function main() {
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    const data = generateRandomData();
